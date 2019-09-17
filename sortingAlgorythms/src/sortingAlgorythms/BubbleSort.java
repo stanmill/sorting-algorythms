6af30d3b18fa83c1e131e72bcb2ec457a7ff59f7
stanmill <stanley.pena29@gmail.com>
2019-09-16 23:48:20 -0400

    Update BubbleSort.java

--- sortingAlgorythms/src/sortingAlgorythms/BubbleSort.java
+++ sortingAlgorythms/src/sortingAlgorythms/BubbleSort.java
@@ -22,7 +22,7 @@ public class BubbleSort {
 		for(int i= 0; i<arr.length-1; i++) {
 			
 			for(int j = 0; j<arr.length-i-1; j++) {
-				
+				// if the smaller index has a bigger value then swap
 				if(arr[j] > arr[j+1]) {
 					
 					int temp = arr[j];
